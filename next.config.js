4386955d79521e9dfff9d27ea8736f19036ef603
carrabre <40183833+carrabre@users.noreply.github.com>
2023-11-09 11:44:53 -0800

    updated next config url

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-// const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    output: 'export',
-    unoptimized: true,
-    images: { unoptimized: true },
-    reactStrictMode: true,
-    trailingSlash: true,
-    // assetPrefix: isProd ? '/k51qzi5uqu5dg7k28ndfcytbvmv1rfigg75v4g053782p6shax4uoofeq5n8ba' : '',
-    // basePath: isProd ? '/k51qzi5uqu5dg7k28ndfcytbvmv1rfigg75v4g053782p6shax4uoofeq5n8ba' : '',
-  };
-  
-  module.exports = nextConfig;
+  output: 'export',
+  unoptimized: true,
+  images: { unoptimized: true },
+  reactStrictMode: true,
+  trailingSlash: true,
+  assetPrefix: isProd ? 'https://greenteadao.eth.limo' : '',
+  basePath: isProd ? 'https://greenteadao.eth.limo' : '',
+};
+
+module.exports = nextConfig;
