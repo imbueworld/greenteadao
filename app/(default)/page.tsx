4eb3aa98dec6ecd9975c6b3a535a7ae42bcc5a11
carrabre <40183833+carrabre@users.noreply.github.com>
2023-11-09 15:10:18 -0800

    removed unnecessary code

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -3,19 +3,13 @@ export const metadata = {
   description: 'Welcome to Green Tea Dao – an oasis for the tea enthusiast and the curious alike. Immerse yourself in the verdant world of green tea, where tradition meets tranquility.',
 }
 
-// import Hero from '@/components/hero'
-import Features from '@/components/features'
 import Newsletter from '@/components/newsletter'
 import Zigzag from '@/components/zigzag'
-// import Testimonials from '@/components/testimonials'
 
 export default function Home() {
   return (
     <>
-      {/* <Hero /> */}
-      {/* <Features /> */}
       <Zigzag />
-      {/* <Testimonials /> */}
       <Newsletter />
     </>
   )
