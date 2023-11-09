e269679dd51ccf4704fe1a11705f56a921394ba8
carrabre <40183833+carrabre@users.noreply.github.com>
2023-11-09 11:51:51 -0800

    update next config

--- next.config.js
+++ next.config.js
@@ -4,12 +4,11 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  unoptimized: true,
   images: { unoptimized: true },
   reactStrictMode: true,
   trailingSlash: true,
-  assetPrefix: isProd ? 'https://greenteadao.eth.limo' : '',
-  basePath: isProd ? 'https://greenteadao.eth.limo' : '',
+  assetPrefix: isProd ? '/k51qzi5uqu5dg7k28ndfcytbvmv1rfigg75v4g053782p6shax4uoofeq5n8ba' : '',
+  basePath: isProd ? '/k51qzi5uqu5dg7k28ndfcytbvmv1rfigg75v4g053782p6shax4uoofeq5n8ba' : '',
 };
 
 module.exports = nextConfig;
