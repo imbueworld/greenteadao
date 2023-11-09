5d07e82f2d85e5f774309b1088ff9d177685ad07
carrabre <40183833+carrabre@users.noreply.github.com>
2023-11-09 11:55:59 -0800

    update next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   reactStrictMode: true,
   trailingSlash: true,
-  assetPrefix: isProd ? '/k51qzi5uqu5dg7k28ndfcytbvmv1rfigg75v4g053782p6shax4uoofeq5n8ba' : '',
-  basePath: isProd ? '/k51qzi5uqu5dg7k28ndfcytbvmv1rfigg75v4g053782p6shax4uoofeq5n8ba' : '',
 };
 
 module.exports = nextConfig;
