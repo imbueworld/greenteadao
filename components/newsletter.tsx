aa45060dfca7441c6179662b8c0406cb091fa30e
carrabre <40183833+carrabre@users.noreply.github.com>
2023-11-20 17:34:03 -0800

    updated points

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -1,42 +1,59 @@
+import Image from "next/image";
 
-import Image from 'next/image'
-
-import box from '../public/images/box.png'
+import box from "../public/images/box.png";
 
 export default function Newsletter() {
   return (
     <section>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6" >
-
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
         {/* CTA box */}
-        <div className="relative bg-gray-900 py-10 px-8 md:py-16 md:px-12 border border-gray-700" data-aos="fade-up">
-
-
+        <div
+          className="relative bg-gray-900 py-10 px-8 md:py-16 md:px-12 border border-gray-700"
+          data-aos="fade-up"
+        >
           {/* Image section */}
           <div className="w-full flex justify-center mb-6">
-            <Image src={box} alt="Descriptive Alt Text" className="max-w-xs sm:max-w-md lg:max-w-lg" />
+            <Image
+              src={box}
+              alt="Descriptive Alt Text"
+              className="max-w-xs sm:max-w-md lg:max-w-lg"
+            />
           </div>
           {/* <div className="relative flex flex-col lg:flex-row justify-between items-center"> */}
 
-            {/* CTA content */}
-            <div className="mb-6 lg:mr-16 lg:mb-0 text-center lg:text-left lg:w-1/2">
-              <h3 className="h3 text-white mb-2">Want to purchase the green tea collection?</h3>
-              <p className="text-gray-200 text-lg">Click the "get tea" button below to buy the 47 prefectures collection in Japan.</p>
-              <br></br>
-              <p className="text-gray-200 text-lg">It will also include:</p>
-              <br></br>
-              <ul className="text-gray-200 text-lg list-disc pl-5">
-                <li>Custom US Made Wooden Box With Japan Map Engraved on Top</li>
-                <li>A matcha kit</li>
-
-              </ul>
-            </div>
-            <div className="w-full flex justify-center mt-6">
-              <a href="https://mint.greenteadao.eth.limo/" target="_blank" rel="noopener noreferrer">
-                <button className="btn text-gray-600 bg-gray-100 hover:bg-white shadow">Get Tea!</button>
-              </a>
-            </div>
-            {/* CTA form
+          {/* CTA content */}
+          <div className="mb-6 lg:mr-16 lg:mb-0 text-center lg:text-left lg:w-1/2">
+            <h3 className="h3 text-white mb-2">
+              Want to purchase the green tea collection?
+            </h3>
+            <p className="text-gray-200 text-lg">
+              Click the "get tea" button below to buy the 47 prefectures
+              collection in Japan.
+            </p>
+            <br></br>
+            <p className="text-gray-200 text-lg">It will also include:</p>
+            <br></br>
+            <ul className="text-gray-200 text-lg list-disc pl-5">
+              <li>Custom US Made Wooden Box With Japan Map Engraved on Top</li>
+              <li>
+                A ceremonial grade matcha kit + traditional japanese chasen
+              </li>
+              <li>Unique NFT of the map of Japan(with NFT purchase)</li>
+              <li>Early Access to future products (with NFT purchase)</li>
+            </ul>
+          </div>
+          <div className="w-full flex justify-center mt-6">
+            <a
+              href="https://mint.greenteadao.eth.limo/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btn text-gray-600 bg-gray-100 hover:bg-white shadow">
+                Get Tea!
+              </button>
+            </a>
+          </div>
+          {/* CTA form
             <form className="w-full lg:w-1/2" action="https://submit-form.com/IIrzyJ0r">
               <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:max-w-none">
                 <input type="text" id="name" name="name" placeholder="Name" className="w-full appearance-none bg-gray-700 border border-gray-500 focus:border-gray-300 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-400" required="" />
@@ -48,10 +65,8 @@ export default function Newsletter() {
             </form> */}
 
           {/* </div> */}
-
         </div>
-
       </div>
     </section>
-  )
+  );
 }
